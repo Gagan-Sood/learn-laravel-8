d69d49f3aca6025d3722efc9f76bf8d34826f92d
Gagan Sood <gagansood777@gmail.com>
2022-08-31 12:35:32 +0530

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('hello');
+    // return redirect('about');
+    return view('welcome');
 });
+Route::view("about", "/about");
+Route::view("contact", "/contact");
